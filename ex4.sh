00beef5abccc7d8513b2cd59b7f2863cb1ecfc0e
rakeshyaramala <114555190+rakeshyaramala@users.noreply.github.com>
2022-11-09 12:15:04 +0530

    #smallest of three numbers

---
+++ ex4.sh
@@ -0,0 +1,25 @@
+#smallest of three numbers
+echo "enter three numbers"
+read a b c 
+if [ $a -lt $b ]
+then
+echo "$a is smallest"
+elif [ $b -lt $c ]
+then
+echo "$b is smallest"
+elif [ $c -lt $a ]
+then
+echo "$c is smallest"
+fi#smallest of three numbers
+echo "enter three numbers"
+read a b c 
+if [ $a -lt $b ]
+then
+echo "$a is smallest"
+elif [ $b -lt $c ]
+then
+echo "$b is smallest"
+elif [ $c -lt $a ]
+then
+echo "$c is smallest"
+fi
